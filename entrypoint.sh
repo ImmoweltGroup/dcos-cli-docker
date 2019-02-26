f32c65d80a6587577dbf4e036b16d0c670d74385
Foerster, Leroy <leroy.foerster@immonet.de>
2019-02-26 16:07:15 +0100

    Use new method to setup cluster URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -eu
 
-./dcos config set core.dcos_url "${DCOS_SERVER_URL?Environment variable has to be set to a DCOS server URL.}"
+./dcos cluster setup "${DCOS_SERVER_URL?Environment variable has to be set to a DCOS server URL.}"
 exec ./dcos "$@"
